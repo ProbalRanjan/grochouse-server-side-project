7d1dd6911d0417608ed2287e26155cef1bb02277
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-05-08 09:10:51 +0600

    delete operation added

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ async function run() {
             res.send(result);
         })
 
+        // DELETE Data from Database
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
         // await client.close();
